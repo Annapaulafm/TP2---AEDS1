f63a306e42b6ee02c624413b0b3d098e565dbf2d
Math1001gs <matheus.santos4@ufv.br>
2025-10-21 21:15:42 -0300

    Update Drone.h

--- Drone.h
+++ Drone.h
@@ -1,7 +1,7 @@
 #ifndef Drone
 #define Drone
-#include "ListaPacote.h"
-#include "Galpao.h"
+#include "../ListaPacote/ListaPacote.h"
+#include "../Galpao/Galpao.h"
 
 // Criação do Drone //
 typedef struct Drone
@@ -15,8 +15,7 @@ typedef struct Drone
 // Operações do Drone //
 TDrone* InicializarDrone(int PesoMaximo);
 void CarregarDrone(TDrone *drone, TGalpao* galpao);
-void Entregas(TDrone *drone, FILE* arquivo2);
+void Entregas(TDrone *drone, int i, FILE* arquivo2);
 void Imprimir(TDrone *drone, int i, FILE* arquivo2);
-void ProblemaCarregamento(TDrone *drone, TGalpao* galpao, int n);
 
 #endif
